0dc57d12dedb4c280880e14ea64cd0233f2191dd
NamalChandrasekara <85227162+NamalChandrasekara@users.noreply.github.com>
2025-02-25 17:13:55 +0530

    Login_Form

--- BACKEND/p_backend/myapp/models.py
+++ BACKEND/p_backend/myapp/models.py
@@ -1,3 +1,13 @@
 from django.db import models
-
+from django.contrib.auth.models import User
 # Create your models here.
+
+class Reminder(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    medicine_name = models.CharField(max_length=255)
+    dosage = models.CharField(max_length=100)
+    time =models.TimeField()
+
+    def __str__(self):
+        return f"{self.medicine_name} - {self.dosage}"
+    
